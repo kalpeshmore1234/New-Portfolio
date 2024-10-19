dd3e1e494c7ba9288712a57fb10c239247e935c1
Kalpesh More <90396306+kalpeshmore1234@users.noreply.github.com>
2024-10-19 18:55:47 +0530

    add Food App

--- src/App.jsx
+++ src/App.jsx
@@ -1087,6 +1087,102 @@ function App() {
           </div>
         </a>
       </div>
+
+
+
+      <div
+        className="flex flex-col sm:flex-row gap-4 mt-12 sm:gap-[80px] items-center sm:mt-20 relative"
+      >
+        <div
+          className="h-[1px] left-1/4 right-1/2 bg-[#0FFF50] absolute top-1/2 hidden sm:block"
+        ></div>
+        <div
+          className="w-4 h-4 rounded-full border-[3px] border-[#fc815c] absolute left-1/2 -translate-x-1/2 bg-[#111] z-10 hover:scale-110 ease-in-out duration-100 hidden sm:block"
+        ></div>
+        <a
+          href="https://mern-food-ordering-app-frontend-raez.onrender.com/"
+          className="flex w-full relative justify-center sm:justify-start"
+          target="_blank"
+        >
+          <div
+            className="flex flex-col items-center relative group hover:scale-105 ease-in-out sm:mr-auto duration-200"
+          >
+            <img
+              className="max-w-[400px] w-full mr-auto relative z-10 drop-shadow-[0_0px_60px_rgba(125,231,20,0.6)]"
+              src="https://res.cloudinary.com/db0oa8ou5/image/upload/v1729344164/MERN_Food_App_twlcqw.png"
+              alt=""
+            />
+            <span
+              className="flex group-hover:-top-14 ease-jump duration-200 bg-[#fc815c] sm:absolute left-1/2 sm:-translate-x-1/2 top-5 px-2 py-1 text-sm sm:text-base mt-2 rounded w-max items-center gap-1 after:hidden sm:after:block after:w-4 after:h-4 after:bg-inherit after:absolute after:left-1/2 after:-translate-x-1/2 after:rotate-45 after:-bottom-2"
+              >Food Ordeing App
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                className="feather feather-external-link"
+              >
+                <path
+                  d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"
+                ></path>
+                <polyline points="15 3 21 3 21 9"></polyline>
+                <line x1="10" y1="14" x2="21" y2="3"></line>
+              </svg>
+            </span>
+          </div>
+        </a>
+
+        <div className="w-full">
+          <h3 className="text-[#fc815c] font-bold text-2xl md:text-4xl">
+            Food Ordering App
+          </h3>
+          <span className="text-[#fc815c] text-base md:text-lg"
+            >(Food Ordering App)</span
+          >
+          <p className="text-justify text-sm md:text-base mt-2">
+          Developed a responsive full stack food ordering app using MERN. It has all the functionalities including authentication, addToCart, creating account as restaurant owner, creating an account as a customer, Order checking, Order status and also Payment completion using stripe.
+          </p>
+
+          <ul className="flex flex-wrap gap-2 mt-2">
+            <li
+              className="border rounded-[50px] border-[#999] px-[10px] py-[5px] text-sm md:text-base"
+            >
+              #react.js
+            </li>
+            
+            <li
+              className="border rounded-[50px] border-[#999] px-[10px] py-[5px] text-sm md:text-base"
+            >
+              #tailwind css
+            </li>
+      
+            <li
+              className="border rounded-[50px] border-[#999] px-[10px] py-[5px] text-sm md:text-base"
+            >
+              #node.js
+            </li>
+            <li
+              className="border rounded-[50px] border-[#999] px-[10px] py-[5px] text-sm md:text-base"
+            >
+              #express.js
+            </li>
+            <li
+              className="border rounded-[50px] border-[#999] px-[10px] py-[5px] text-sm md:text-base"
+            >
+              #mongodb
+            </li>
+          </ul>
+        </div>
+      </div>
+
+
+
+      
       
 
       
