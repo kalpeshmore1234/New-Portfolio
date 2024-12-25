19916db4941f345e0b08fdb768dd7e804985b30f
Kalpesh More <90396306+kalpeshmore1234@users.noreply.github.com>
2024-12-25 20:56:58 +0530

    add unsplash project

--- src/App.jsx
+++ src/App.jsx
@@ -1181,6 +1181,93 @@ function App() {
       </div>
 
 
+      <div
+        className="flex flex-col sm:flex-row gap-4 mt-12 sm:gap-[80px] items-center sm:mt-20 relative"
+      >
+        <div className="order-2 sm:order-1 w-full">
+          <h3 className="text-[#CC00CC] font-bold text-2xl md:text-4xl">
+            Unsplash Images
+          </h3>
+          <span className="text-[#CC00CC] text-base md:text-lg"
+            >(Unsplash Images App)</span
+          >
+          <p className="text-justify text-sm md:text-base mt-2">
+          Created a React app using React Query to fetch and display images from the Unsplash API, providing a seamless browsing experience for users to view high-quality images.
+          </p>
+
+          <ul className="flex flex-wrap gap-2 mt-2">
+            <li
+              className="border rounded-[50px] border-[#999] px-[10px] py-[5px] text-sm md:text-base"
+            >
+              #react.js
+            </li>
+            
+            <li
+              className="border rounded-[50px] border-[#999] px-[10px] py-[5px] text-sm md:text-base"
+            >
+              #css
+            </li>
+            <li
+              className="border rounded-[50px] border-[#999] px-[10px] py-[5px] text-sm md:text-base"
+            >
+              #javascript
+            </li>
+            <li
+              className="border rounded-[50px] border-[#999] px-[10px] py-[5px] text-sm md:text-base"
+            >
+              #react_query
+            </li>
+          
+        
+           
+          </ul>
+        </div>
+        <div
+          className="h-[1px] left-1/2 right-1/4 bg-[#0FFF50] absolute top-1/2 hidden sm:block"
+        ></div>
+        <div
+          className="w-4 h-4 rounded-full border-[3px] border-[#47afa1] absolute left-1/2 -translate-x-1/2 bg-[#111] z-10 hover:scale-110 ease-in-out duration-100 hidden sm:block"
+        ></div>
+        <a
+          href="https://unsplash-images123.netlify.app/"
+          className="order-1 sm:order-2 flex w-full relative justify-center sm:justify-start"
+          target="_blank"
+        >
+          <div
+            className="flex flex-col items-center relative group sm:hover:scale-105 ease-in-out duration-200 sm:ml-auto"
+          >
+            <img
+              className="max-w-[400px] w-full relative z-10 drop-shadow-[0_0px_60px_rgba(125,231,20,0.6)]"
+              src="https://res.cloudinary.com/db0oa8ou5/image/upload/v1735140115/Unsplash_Images_1_dqp5wv.png"
+              alt=""
+            />
+            <span
+              className="flex group-hover:-top-14 ease-jump duration-200 bg-[#CC00CC] sm:absolute left-1/2 sm:-translate-x-1/2 top-5 px-2 py-1 text-sm sm:text-base mt-2 rounded w-max items-center gap-1 after:hidden sm:after:block after:w-4 after:h-4 after:bg-inherit after:absolute after:left-1/2 after:-translate-x-1/2 after:rotate-45 after:-bottom-2"
+              >Unsplash Images
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                className="feather feather-external-link"
+              >
+                <path
+                  d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"
+                ></path>
+                <polyline points="15 3 21 3 21 9"></polyline>
+                <line x1="10" y1="14" x2="21" y2="3"></line>
+              </svg>
+            </span>
+          </div>
+        </a>
+      </div>
+
+
 
       
       
